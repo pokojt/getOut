d0340abf38d1d4c89b23a8a4f5a7d00cd11fbd11
Taylor Pokoj <pokojt@gmail.com>
2016-05-16 12:04:55 -0700

    Try to hook up firebase

--- app/models/category.js
+++ app/models/category.js
@@ -1,6 +1,6 @@
 import Model from 'ember-data/model';
 
-export default Model.extend({
+export default DS.Model.extend({
   name: DS.attr(),
   events: DS.hasMany('user-event', {async: true})
 });
--- app/models/user-event.js
+++ app/models/user-event.js
@@ -1,14 +1,16 @@
 import Model from 'ember-data/model';
 
-export default Model.extend({
+export default DS.Model.extend({
   eventName: DS.attr(),
   date: DS.attr('date'),
   time: DS.attr('time'),
   location: DS.attr(),
   image: DS.attr(),
-  category: DS.hasMany('category', {async:true}),
+  // category: DS.hasMany('category', {async:true}),
   description: DS.attr(),
   price: DS.attr(),
+  urban: DS.attr('boolean', {defaultValue: true}),
+  wild: DS.attr('boolean', {defaultValue: true}),
   // comments: DS.hasMany('comment'),
   // user: DS.belongsTo('user')
 });
