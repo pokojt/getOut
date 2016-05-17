b51cbe57d3a851afec6261a0b7c907d22b37b7c9
Taylor Pokoj <pokojt@gmail.com>
2016-05-17 14:59:05 -0700

    Add multiple models to home page

--- app/routes/home-page.js
+++ app/routes/home-page.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('event');
+    return Ember.RSVP.hash ({
+      yourEvents: this.store.findAll('event'),
+      friendEvents: this.store.findAll('admin-event'),
+    });
   },
   actions: {
     showNav: function() {
@@ -10,3 +13,17 @@ export default Ember.Route.extend({
     }
   }
 });
+
+
+// import Ember from 'ember';
+//
+// export default Ember.Route.extend({
+//   model() {
+//     return this.store.findAll('event');
+//   },
+//   actions: {
+//     showNav: function() {
+//       $(".navigation").toggle('hidden');
+//     }
+//   }
+// });
